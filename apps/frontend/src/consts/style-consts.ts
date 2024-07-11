83f1e052cf0cb499559613bcba3614294e047186
mateusz-kolasa <mateusz.kolasa97@gmail.com>
2024-07-11 19:32:05 +0200

    fix: correct list max height calculation

--- apps/frontend/src/consts/style-consts.ts
+++ apps/frontend/src/consts/style-consts.ts
@@ -7,4 +7,4 @@ export const APP_SHELL_MAIN_HEIGHT =
 export const APP_SHELL_SUBHEADER_MAIN_HEIGHT = `calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px)
  - var(--app-shell-padding, 0px) * 2 - ${SUB_HEADER_HEIGHT + 1}px)`
 export const LIST_MAX_HEIGHT = `calc(100vh - var(--app-shell-header-height, 0px) - var(--app-shell-footer-height, 0px)
- - var(--app-shell-padding, 0px) * 4- ${SUB_HEADER_HEIGHT + 1}px)`
+ - var(--app-shell-padding, 0px) * 4 - ${SUB_HEADER_HEIGHT + 1}px)`
